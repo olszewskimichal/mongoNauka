cda923a92e835f509c96fb96b8b6431449639811
olszewskimichal <olszewskimichal@outlook.com>
2018-12-17 18:46:27 +0100

    add some test about users - using mongoDB and kotlin

---
+++ src/main/kotlin/pl/michal/olszewski/mongonauka/users/UserRepository.kt
@@ -0,0 +1,21 @@
+package pl.michal.olszewski.mongonauka.users
+
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : MongoRepository<User, String> {
+
+    fun findByNameStartingWith(regex: String): List<User>
+    fun findByNameEndingWith(regex: String): List<User>
+
+    @Query("{ 'age' : { \$gt: ?0, \$lt: ?1 } }")
+    fun findUsersByAgeBetween(ageGT: Int, ageLT: Int): List<User>
+
+    @Query("{name : {\$ne : ?0}}")
+    fun findByNameNotEqual(countryName: String): List<User>
+
+    @Query("{name : ?0, age : ?1}")
+    fun findUserByNameAndAge(name: String, age: Int): List<User>
+}
---
+++ src/test/kotlin/pl/michal/olszewski/mongonauka/UserRepositoryTest.groovy
@@ -0,0 +1,77 @@
+package pl.michal.olszewski.mongonauka
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+import org.springframework.data.mongodb.core.MongoTemplate
+import pl.michal.olszewski.mongonauka.users.User
+import pl.michal.olszewski.mongonauka.users.UserRepository
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@DataMongoTest
+@Unroll
+class UserRepositoryTest extends Specification {
+
+    @Autowired
+    UserRepository repository
+
+    @Autowired
+    private MongoTemplate mongoTemplate
+
+    def setup() {
+        repository.deleteAll()
+    }
+
+    def 'should find by name starting with A'() {
+        given:
+        mongoTemplate.insert(new User("Adam", 10))
+        when:
+        def result = repository.findByNameStartingWith("A")
+        then:
+        result.size() == 1
+    }
+
+    def 'should find by name ending with ał'() {
+        given:
+        mongoTemplate.insert(new User("Michał", 10))
+        when:
+        def result = repository.findByNameEndingWith("ał")
+        then:
+        result.size() == 1
+    }
+
+
+    def 'should find users by age between 10 and 15'() {
+        given:
+        mongoTemplate.insert(new User("Michał 10", 10))
+        mongoTemplate.insert(new User("Michał 12", 12))
+        mongoTemplate.insert(new User("Michał 15", 15))
+        mongoTemplate.insert(new User("Michał 20", 20))
+        when:
+        def result = repository.findUsersByAgeBetween(10, 15)
+        then:
+        result.size() == 1
+    }
+
+    def 'should find users with not equal name'() {
+        given:
+        mongoTemplate.insert(new User("Adam", 14))
+        mongoTemplate.insert(new User("Michał", 14))
+        mongoTemplate.insert(new User("Adamek", 14))
+        when:
+        def notEqual = repository.findByNameNotEqual("Adam")
+        then:
+        notEqual.size() == 2
+    }
+
+    def 'should find user by name and age'() {
+        given:
+        mongoTemplate.insert(new User("Michał", 14))
+        mongoTemplate.insert(new User("Michał", 15))
+        when:
+        def users = repository.findUserByNameAndAge "Michał", 14
+        then:
+        users.size()==1
+    }
+
+}
