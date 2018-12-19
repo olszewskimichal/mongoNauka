054b481b7975062e418f78d63c6547aec4746d9a
olszewskimichal <olszewskimichal@outlook.com>
2018-12-19 18:11:11 +0100

    clear code

--- src/main/kotlin/pl/michal/olszewski/mongonauka/MongonaukaApplication.kt
+++ src/main/kotlin/pl/michal/olszewski/mongonauka/MongonaukaApplication.kt
@@ -1,15 +1,11 @@
 package pl.michal.olszewski.mongonauka
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import pl.michal.olszewski.mongonauka.customer.CustomerRepository
 
 
 @SpringBootApplication
 class MongonaukaApplication {
-    @Autowired
-    private lateinit var repository: CustomerRepository
 
     companion object {
         @JvmStatic
@@ -17,34 +13,4 @@ class MongonaukaApplication {
             SpringApplication.run(MongonaukaApplication::class.java, *args)
         }
     }
-
-    /*@Throws(Exception::class)
-    override fun run(vararg args: String) {
-
-        repository.deleteAll()
-
-        // save a couple of customers
-        repository.save(Customer(null, "Alice", "Smith"))
-        repository.save(Customer(null, "Bob", "Smith"))
-
-        // fetch all customers
-        println("Customers found with findAll():")
-        println("-------------------------------")
-        for (customer in repository.findAll()) {
-            println(customer)
-        }
-        println()
-
-        // fetch an individual customer
-        println("Customer found with findByFirstName('Alice'):")
-        println("--------------------------------")
-        println(repository.findByFirstName("Alice"))
-
-        println("Customers found with findByLastName('Smith'):")
-        println("--------------------------------")
-        for (customer in repository.findByLastName("Smith")) {
-            println(customer)
-        }
-
-    }*/
 }
